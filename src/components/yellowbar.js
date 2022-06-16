bd8026b3a504a4b4124a094201a3209505e2d440
ekc0621 <cek0621@naver.com>
2022-06-16 21:37:53 +0900

    third commit

---
+++ src/components/yellowbar.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function yellowbar(){
+    return(
+        <div className = "yellowbar">
+            <div className="ABC_logo">ABC-MART</div>
+            <div className="drop_down"></div>
+        </div>
+    )
+}
