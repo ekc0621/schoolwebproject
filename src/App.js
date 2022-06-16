9810d7f627a1e12b5116f9ee1db9b5f4d9618773
ekc0621 <cek0621@naver.com>
2022-06-16 16:57:49 +0900

    second commit

--- src/App.js
+++ src/App.js
@@ -1,15 +1,56 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <nav className='container'>
       <div className='RedBar'>
-        <li>
-          <a href="https://www.facebook.com/abcmartkorea" target ="blank">
-            FACEBOOK
-          </a>
-        </li>
+        <div className = "sns">
+          <li>
+            <a className = "Shop" href="https://www.a-rt.com/" target ="blank">
+            <i class="fa fa-shopping-bag" aria-hidden="true"></i>
+              SHOPPING MALL
+            </a>
+          </li>
+          <li>
+            <a clssName = "Facebook" href="https://www.facebook.com/abcmartkorea" target ="blank">
+              <i class="fa fa-facebook-official" aria-hidden="true"></i>
+              FACEBOOK
+            </a>
+          </li>
+          <li>
+            <a className = "Instargram" href="https://www.instagram.com/accounts/login/?next=/abcmartkr/" target ="blank">
+              <i class="fa fa-instagram" aria-hidden="true"></i>
+              INSTAGRAM
+            </a>
+          </li>
+        </div>
+        <div className ="shoes">
+          <li>
+            <a className = "Danner" href="http://dannerkorea.co.kr/" target = "blank">
+              DANNER
+            </a>
+          </li>
+          <li>
+            <a className = "Sperry" href="https://sperrykorea.co.kr/" target = "blank">
+              SPERRY
+            </a>
+          </li>
+          <li>
+            <a className = "Saucony" href="https://www.saucony.com/en/home/" target = "blank">
+              SAUCONY
+            </a>
+          </li>
+          <li>
+            <a className = "Nuovo" href="https://abcmart.a-rt.com/product/brand/page/main?brandNo=000081&page=1" target = "blank">
+              NUOVO
+            </a>
+          </li>
+          <li>
+            <a className = "Hawkins" href="http://hawkinskorea.co.kr/" target = "blank">
+              HAWKINS
+            </a>
+          </li>
+        </div>
       </div>
     </nav>
   );
