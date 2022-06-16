efb8b1be604c038bd69f54ef677d67fade83957c
ekc0621 <106240897+ekc0621@users.noreply.github.com>
2022-06-16 13:50:59 +0900

    init

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <nav className='container'>
+      <div className='RedBar'>
+        <li>
+          <a href="https://www.facebook.com/abcmartkorea" target ="blank">
+            FACEBOOK
+          </a>
+        </li>
+      </div>
+    </nav>
+  );
+}
+
+export default App;
