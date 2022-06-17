c816aa305607b1b5695e9051b02046549f9219bc
ekc0621 <cek0621@naver.com>
2022-06-17 14:48:38 +0900

    update

--- src/components/yellowbar.js
+++ src/components/yellowbar.js
@@ -4,7 +4,29 @@ export default function yellowbar(){
     return(
         <div className = "yellowbar">
             <div className="ABC_logo">ABC-MART</div>
-            <div className="drop_down"></div>
+            <div className="drop_down">                
+                <ul className="main-menu">
+                    <li>
+                        RECRUIT
+                    </li>
+                    <li>
+                        SUPPORT
+                        <ul className="sup_sub">
+                            <li><a href = ""></a></li>
+                        </ul>
+                    </li>
+                    <li>
+                        STORE
+                    </li>
+                    <li>                            
+                        MEDIA
+                    </li>
+                    <li>
+                        <a href="https://abcmartkorea.saramin.co.kr/main/index">COMPANY</a>
+                    </li>
+                </ul>
+                
+            </div>
         </div>
     )
 }
